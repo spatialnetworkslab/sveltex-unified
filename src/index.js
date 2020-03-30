765fa54b7d5b2604b73693734f733aa12bd323fa
Ate Poorthuis <atepoorthuis@gmail.com>
2020-03-30 19:41:25 +0800

    chore: remove csb-plus
    porting over to florence site instead

--- src/index.js
+++ src/index.js
@@ -25,8 +25,6 @@ import escapeCurlies from './escapeCurlies.js'
 import containers from 'remark-containers'
 import csbBlock from './codeSandBoxBlock.js'
 import csbUpload from './codeSandBoxUpload.js'
-import csbBlockPlus from './codeSandBoxBlockPlus.js'
-import csbUploadPlus from './codeSandBoxUploadPlus.js'
 import solution from './solutions.js'
 import solutionHide from './solutionhide.js'
 
@@ -70,11 +68,6 @@ export const defaultProcessor = unified()
         transform: csbBlock
       },
       {
-        type: 'codesandboxplus',
-        element: 'div',
-        transform: csbBlockPlus
-      },
-      {
         type: 'solution',
         element: 'div',
         transform: solution
@@ -83,7 +76,6 @@ export const defaultProcessor = unified()
   })
   .use(solutionHide)
   .use(csbUpload)
-  .use(csbUploadPlus)
   .use(svelteInline)
   .use(svelteBlock)
   .use(svelteElementBlock)
--- test/codesandboxplus/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-
-$:console.log("hello world")
-</script>
-
-<h1>Hello world</h1>
-<h2>Hello world</h2>
-<h3>Hello world</h3>
-<h4>Hello world</h4>
